b1103a0b908593693eb8d3d818db0e63e49510e2
palamar-iy <ira_palamar@mail.ru>
2018-06-05 17:45:33 +0300

    Test4

--- src/Shell.java
+++ src/Shell.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Shell {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("тест тест тес55т");
+        System.out.print("тест 123121");
+        System.out.print("тест 8789787987");
         while(true) {
             System.out.print("введите команду>");
             String str = scanner.nextLine();
