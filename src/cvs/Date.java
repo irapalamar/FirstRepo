0ecfb57c81807af60a87402643eaa04f7848e4fc
irapalamar <ira_palamar@mail.ru>
2018-07-24 09:44:35 +0300

    Test

---
+++ src/cvs/Date.java
@@ -0,0 +1,11 @@
+package cvs;
+
+/**
+ * Created by sbt-palamar-iy on 24.07.2018.
+ */
+public @interface Date {
+    int month() default 01;
+    int day() default 01;
+    int year() default 1990;
+    String previous();
+}
---
+++ src/cvs/Version.java
@@ -0,0 +1,11 @@
+package cvs;
+
+/**
+ * Created by sbt-palamar-iy on 24.07.2018.
+ */
+public @interface Version {
+    int value();
+    String creationDate();
+    String author();
+
+}
